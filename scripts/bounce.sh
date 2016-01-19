9aa65934c418428b2cdccdf2180a1fd5c85926fc
Sean Robertson <sprobertson@gmail.com>
2016-01-19 15:35:42 -0800

    bounce login

--- scripts/bounce.sh
+++ scripts/bounce.sh
@@ -1,5 +1,6 @@
 PORT=${1:-2889}
 BASE=${2:-static}
-rm $BASE/{css,js}/app.bounced.*
+rm $BASE/{css,js}/{app,login}.bounced.*
 curl -o $BASE/js/app.bounced.js localhost:$PORT/js/app.js -H "X-Enable-Bouncer: true"
+curl -o $BASE/js/login.bounced.js localhost:$PORT/js/login.js -H "X-Enable-Bouncer: true"
 curl -o $BASE/css/app.bounced.css localhost:$PORT/css/app.css -H "X-Enable-Bouncer: true"
